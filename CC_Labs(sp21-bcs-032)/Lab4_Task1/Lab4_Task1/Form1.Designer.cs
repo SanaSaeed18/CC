b49a327d66b3f63812f2b8a76afe773c0a033651
SanaSaeed18 <161746607+SanaSaeed18@users.noreply.github.com>
2024-04-04 12:21:27 -0700

    Add files via upload

---
+++ CC_Labs(sp21-bcs-032)/Lab4_Task1/Lab4_Task1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Lab4_Task1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SourceCodeTextBox = new System.Windows.Forms.TextBox();
+            this.AnalyzeButton = new System.Windows.Forms.Button();
+            this.TokensListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SourceCodeTextBox
+            // 
+            this.SourceCodeTextBox.BackColor = System.Drawing.Color.LightGray;
+            this.SourceCodeTextBox.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SourceCodeTextBox.Location = new System.Drawing.Point(12, 144);
+            this.SourceCodeTextBox.Multiline = true;
+            this.SourceCodeTextBox.Name = "SourceCodeTextBox";
+            this.SourceCodeTextBox.Size = new System.Drawing.Size(260, 148);
+            this.SourceCodeTextBox.TabIndex = 0;
+            // 
+            // AnalyzeButton
+            // 
+            this.AnalyzeButton.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.AnalyzeButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AnalyzeButton.ForeColor = System.Drawing.Color.White;
+            this.AnalyzeButton.Location = new System.Drawing.Point(293, 190);
+            this.AnalyzeButton.Name = "AnalyzeButton";
+            this.AnalyzeButton.Size = new System.Drawing.Size(132, 31);
+            this.AnalyzeButton.TabIndex = 1;
+            this.AnalyzeButton.Text = "Analyze";
+            this.AnalyzeButton.UseVisualStyleBackColor = false;
+            this.AnalyzeButton.Click += new System.EventHandler(this.AnalyzeButton_Click);
+            // 
+            // TokensListBox
+            // 
+            this.TokensListBox.BackColor = System.Drawing.Color.LightGray;
+            this.TokensListBox.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TokensListBox.ForeColor = System.Drawing.Color.Black;
+            this.TokensListBox.FormattingEnabled = true;
+            this.TokensListBox.ItemHeight = 18;
+            this.TokensListBox.Location = new System.Drawing.Point(461, 144);
+            this.TokensListBox.Name = "TokensListBox";
+            this.TokensListBox.Size = new System.Drawing.Size(282, 148);
+            this.TokensListBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(242, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(274, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Welcome to lexical Analyzer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(416, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(812, 405);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TokensListBox);
+            this.Controls.Add(this.AnalyzeButton);
+            this.Controls.Add(this.SourceCodeTextBox);
+            this.Name = "Form1";
+            this.Text = "Lexical Analyzer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox SourceCodeTextBox;
+        private System.Windows.Forms.Button AnalyzeButton;
+        private System.Windows.Forms.ListBox TokensListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ CC_Labs(sp21-bcs-032)/Lab4_Task1/Lab4_Task1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Lab4_Task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AnalyzeButton_Click(object sender, EventArgs e)
+        {
+            string sourceCode = SourceCodeTextBox.Text;
+            string[] tokens = AnalyzeSourceCode(sourceCode);
+            DisplayTokens(tokens);
+        }
+        private string[] AnalyzeSourceCode(string sourceCode)
+        {
+            List<string> tokens = new List<string>();
+
+            
+            string[] keywords = { "if", "else", "while", "for", "int", "float", "bool", "true", "false" };
+            string identifierPattern = @"\b[a-zA-Z_]\w*\b"; 
+            string numberPattern = @"\b[-+]?\d*\.?\d+\b";
+            string operatorPattern = @"(\+|\-|\*|\/|=|>|<|>=|<=|&|\||%|!|\^|\(|\))";
+            string whitespacePattern = @"\s+";
+
+
+            int pos = 0;
+            while (pos < sourceCode.Length)
+            {
+                if (char.IsLetter(sourceCode[pos]) || sourceCode[pos] == '_')
+                {
+                    Match match = Regex.Match(sourceCode.Substring(pos), identifierPattern);
+                    string token = match.Value;
+                    if (Array.Exists(keywords, k => k == token))
+                    {
+                        tokens.Add($"Keyword: {token}");
+                    }
+                    else
+                    {
+                        tokens.Add($"Identifier: {token}");
+                    }
+                    pos += match.Length;
+                }
+                else if (char.IsDigit(sourceCode[pos]))
+                {
+                    Match match = Regex.Match(sourceCode.Substring(pos), numberPattern);
+                    tokens.Add($"Number: {match.Value}");
+                    pos += match.Length;
+                }
+                else if (Regex.IsMatch(sourceCode[pos].ToString(), operatorPattern))
+                {
+                    tokens.Add($"Operator: {sourceCode[pos]}");
+                    pos++;
+                }
+                else if (Regex.IsMatch(sourceCode[pos].ToString(), whitespacePattern))
+                {
+                    pos++;
+                }
+                else
+                {
+                    
+                    pos++;
+                }
+            }
+
+            return tokens.ToArray();
+        }
+
+
+
+
+
+        private void DisplayTokens(string[] tokens)
+        {
+            TokensListBox.Items.Clear();
+            foreach (string token in tokens)
+            {
+                TokensListBox.Items.Add(token);
+            }
+        }
+    }
+}
