50b68ea60076883e4391717ea6caca53783e9599
SanaSaeed18 <161746607+SanaSaeed18@users.noreply.github.com>
2024-05-31 09:48:01 -0700

    Add files via upload

---
+++ Sana_Saeed_sp21-bcs-032_Lab_terminal/QuestionNo03/RegistrationFormAPP/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace RegistrationFormAPP
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.FirstNameLabel = new System.Windows.Forms.Label();
+            this.LastNameLabel = new System.Windows.Forms.Label();
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.EmailLabel = new System.Windows.Forms.Label();
+            this.MobileNoLabel = new System.Windows.Forms.Label();
+            this.CityLabel = new System.Windows.Forms.Label();
+            this.FirstNameTextBox = new System.Windows.Forms.TextBox();
+            this.LastNameTextBox = new System.Windows.Forms.TextBox();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.EmailTextBox = new System.Windows.Forms.TextBox();
+            this.MobileNoTextBox = new System.Windows.Forms.TextBox();
+            this.CityComboBox = new System.Windows.Forms.ComboBox();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // FirstNameLabel
+            // 
+            this.FirstNameLabel.AutoSize = true;
+            this.FirstNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstNameLabel.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.FirstNameLabel.Location = new System.Drawing.Point(154, 98);
+            this.FirstNameLabel.Name = "FirstNameLabel";
+            this.FirstNameLabel.Size = new System.Drawing.Size(117, 24);
+            this.FirstNameLabel.TabIndex = 0;
+            this.FirstNameLabel.Text = "First Name:";
+            // 
+            // LastNameLabel
+            // 
+            this.LastNameLabel.AutoSize = true;
+            this.LastNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LastNameLabel.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.LastNameLabel.Location = new System.Drawing.Point(154, 138);
+            this.LastNameLabel.Name = "LastNameLabel";
+            this.LastNameLabel.Size = new System.Drawing.Size(114, 24);
+            this.LastNameLabel.TabIndex = 1;
+            this.LastNameLabel.Text = "Last Name:";
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.AutoSize = true;
+            this.UsernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsernameLabel.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.UsernameLabel.Location = new System.Drawing.Point(154, 183);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(111, 24);
+            this.UsernameLabel.TabIndex = 2;
+            this.UsernameLabel.Text = "Username:";
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordLabel.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.PasswordLabel.Location = new System.Drawing.Point(154, 222);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(106, 24);
+            this.PasswordLabel.TabIndex = 3;
+            this.PasswordLabel.Text = "Password:";
+            // 
+            // EmailLabel
+            // 
+            this.EmailLabel.AutoSize = true;
+            this.EmailLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmailLabel.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.EmailLabel.Location = new System.Drawing.Point(154, 263);
+            this.EmailLabel.Name = "EmailLabel";
+            this.EmailLabel.Size = new System.Drawing.Size(68, 24);
+            this.EmailLabel.TabIndex = 4;
+            this.EmailLabel.Text = "Email:";
+            // 
+            // MobileNoLabel
+            // 
+            this.MobileNoLabel.AutoSize = true;
+            this.MobileNoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MobileNoLabel.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.MobileNoLabel.Location = new System.Drawing.Point(154, 302);
+            this.MobileNoLabel.Name = "MobileNoLabel";
+            this.MobileNoLabel.Size = new System.Drawing.Size(112, 24);
+            this.MobileNoLabel.TabIndex = 5;
+            this.MobileNoLabel.Text = "Mobile No:";
+            // 
+            // CityLabel
+            // 
+            this.CityLabel.AutoSize = true;
+            this.CityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CityLabel.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.CityLabel.Location = new System.Drawing.Point(302, 355);
+            this.CityLabel.Name = "CityLabel";
+            this.CityLabel.Size = new System.Drawing.Size(45, 24);
+            this.CityLabel.TabIndex = 6;
+            this.CityLabel.Text = "City:";
+            // 
+            // FirstNameTextBox
+            // 
+            this.FirstNameTextBox.Location = new System.Drawing.Point(327, 102);
+            this.FirstNameTextBox.Name = "FirstNameTextBox";
+            this.FirstNameTextBox.Size = new System.Drawing.Size(290, 20);
+            this.FirstNameTextBox.TabIndex = 7;
+            // 
+            // LastNameTextBox
+            // 
+            this.LastNameTextBox.Location = new System.Drawing.Point(327, 143);
+            this.LastNameTextBox.Name = "LastNameTextBox";
+            this.LastNameTextBox.Size = new System.Drawing.Size(290, 20);
+            this.LastNameTextBox.TabIndex = 8;
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Location = new System.Drawing.Point(327, 183);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(290, 20);
+            this.UsernameTextBox.TabIndex = 9;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(327, 222);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PasswordChar = '*';
+            this.PasswordTextBox.Size = new System.Drawing.Size(290, 20);
+            this.PasswordTextBox.TabIndex = 10;
+            // 
+            // EmailTextBox
+            // 
+            this.EmailTextBox.Location = new System.Drawing.Point(327, 263);
+            this.EmailTextBox.Name = "EmailTextBox";
+            this.EmailTextBox.Size = new System.Drawing.Size(290, 20);
+            this.EmailTextBox.TabIndex = 11;
+            // 
+            // MobileNoTextBox
+            // 
+            this.MobileNoTextBox.Location = new System.Drawing.Point(327, 302);
+            this.MobileNoTextBox.Name = "MobileNoTextBox";
+            this.MobileNoTextBox.Size = new System.Drawing.Size(290, 20);
+            this.MobileNoTextBox.TabIndex = 12;
+            // 
+            // CityComboBox
+            // 
+            this.CityComboBox.FormattingEnabled = true;
+            this.CityComboBox.Items.AddRange(new object[] {
+            "New York",
+            "Los Angeles",
+            "Chicago",
+            "Houston",
+            "Phoenix"});
+            this.CityComboBox.Location = new System.Drawing.Point(367, 360);
+            this.CityComboBox.Name = "CityComboBox";
+            this.CityComboBox.Size = new System.Drawing.Size(84, 21);
+            this.CityComboBox.TabIndex = 13;
+            this.CityComboBox.Text = "Select";
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.BackColor = System.Drawing.Color.LimeGreen;
+            this.RegisterButton.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.RegisterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegisterButton.Location = new System.Drawing.Point(355, 406);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(168, 36);
+            this.RegisterButton.TabIndex = 14;
+            this.RegisterButton.Text = "Register";
+            this.RegisterButton.UseVisualStyleBackColor = false;
+            this.RegisterButton.Click += new System.EventHandler(this.RegisterButton_Click);
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(30, 350);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(0, 13);
+            this.ResultLabel.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(301, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 29);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Registration Form";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(806, 466);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.RegisterButton);
+            this.Controls.Add(this.CityComboBox);
+            this.Controls.Add(this.MobileNoTextBox);
+            this.Controls.Add(this.EmailTextBox);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.UsernameTextBox);
+            this.Controls.Add(this.LastNameTextBox);
+            this.Controls.Add(this.FirstNameTextBox);
+            this.Controls.Add(this.CityLabel);
+            this.Controls.Add(this.MobileNoLabel);
+            this.Controls.Add(this.EmailLabel);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.UsernameLabel);
+            this.Controls.Add(this.LastNameLabel);
+            this.Controls.Add(this.FirstNameLabel);
+            this.Name = "Form1";
+            this.Text = "Registration Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label FirstNameLabel;
+        private System.Windows.Forms.Label LastNameLabel;
+        private System.Windows.Forms.Label UsernameLabel;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.Label EmailLabel;
+        private System.Windows.Forms.Label MobileNoLabel;
+        private System.Windows.Forms.Label CityLabel;
+        private System.Windows.Forms.TextBox FirstNameTextBox;
+        private System.Windows.Forms.TextBox LastNameTextBox;
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.TextBox EmailTextBox;
+        private System.Windows.Forms.TextBox MobileNoTextBox;
+        private System.Windows.Forms.ComboBox CityComboBox;
+        private System.Windows.Forms.Button RegisterButton;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Sana_Saeed_sp21-bcs-032_Lab_terminal/QuestionNo03/RegistrationFormAPP/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace RegistrationFormAPP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            string firstName = FirstNameTextBox.Text;
+            string lastName = LastNameTextBox.Text;
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Text;
+            string email = EmailTextBox.Text;
+            string mobileNo = MobileNoTextBox.Text;
+            string city = CityComboBox.SelectedItem?.ToString();
+
+            string errorMessage = "";
+            bool isValid = true;
+
+            if (!ValidateFirstName(firstName))
+            {
+                isValid = false;
+                errorMessage += "Invalid First Name. ";
+            }
+
+            if (!ValidateLastName(lastName))
+            {
+                isValid = false;
+                errorMessage += "Invalid Last Name. ";
+            }
+
+            if (!ValidateUsername(username))
+            {
+                isValid = false;
+                errorMessage += "Invalid Username. ";
+            }
+
+            if (!ValidatePassword(password))
+            {
+                isValid = false;
+                errorMessage += "Invalid Password. ";
+            }
+
+            if (!ValidateEmail(email))
+            {
+                isValid = false;
+                errorMessage += "Invalid Email. ";
+            }
+
+            if (!ValidateMobileNo(mobileNo))
+            {
+                isValid = false;
+                errorMessage += "Invalid Mobile No. ";
+            }
+
+            if (!ValidateCity(city))
+            {
+                isValid = false;
+                errorMessage += "Invalid City. ";
+            }
+
+            if (isValid)
+            {
+                MessageBox.Show("All inputs are valid!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                ResultLabel.Text = errorMessage;
+            }
+        }
+
+        private bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, @"^[A-Za-z]{1,50}$");
+        }
+
+        private bool ValidateLastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, @"^[A-Za-z]{1,50}$");
+        }
+
+        private bool ValidateUsername(string username)
+        {
+            return Regex.IsMatch(username, @"^[A-Za-z0-9_.]{5,15}$");
+        }
+
+        private bool ValidatePassword(string password)
+        {
+            return Regex.IsMatch(password, @"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$");
+        }
+
+        private bool ValidateEmail(string email)
+        {
+            return Regex.IsMatch(email, @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
+        }
+
+
+        private bool ValidateMobileNo(string mobileNo)
+        {
+            return Regex.IsMatch(mobileNo, @"^\d{10}$");
+        }
+
+        private bool ValidateCity(string city)
+        {
+            return city != null && city != "Select";
+        }
+    }
+}
